a036738a043fe0b37fe2910f216d6f9e868b62a8
Ryan Noonan <ryan.noonan@outlook.com>
2026-01-19 13:02:16 -0500

    add eval on train set

--- scripts/eval_lmlm_multihop.sh
+++ scripts/eval_lmlm_multihop.sh
@@ -1,5 +1,6 @@
 MODEL_PATH=/share/j_sun/lz586/checkpoints/lmlm_multi_hop/Qwen3-1.7B-SFT_ep5_bsz48
 DATASET=hotpotqa
+SPLIT=dev
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -12,6 +13,10 @@ while [[ $# -gt 0 ]]; do
             DATASET="$2"
             shift 2
             ;;
+        --split)
+            SPLIT="$2"
+            shift 2
+            ;;
         *)
             echo "Unknown argument: $1"
             exit 1
@@ -19,11 +24,25 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Set DATABASE_PATH based on dataset
 if [ "${DATASET}" = "hotpotqa" ]; then
-    DATABASE_PATH="/share/j_sun/lmlm_multihop/database/gemini/hotpotqa_validation_42_1000_all_context_database.json"
+    if [ "${SPLIT}" = "dev" ]; then
+        DATABASE_PATH="/share/j_sun/lmlm_multihop/database/gemini/hotpotqa_validation_42_1000_all_context_database.json"
+    elif [ "${SPLIT}" = "train" ]; then
+        echo "Using train set from GRPO"
+        DATABASE_PATH="/share/j_sun/lmlm_multihop/database/gemini/hotpotqa_train_start_idx_82347_nb_8100_database.json"
+    else
+        echo "Error: SPLIT must be either 'train' or 'dev', got '${SPLIT}'"
+        exit 1
+    fi
 elif [ "${DATASET}" = "musique" ]; then
-    DATABASE_PATH="/share/j_sun/lmlm_multihop/database/gemini/musique_validation_42_1000_all_context_database.json"
+    if [ "${SPLIT}" = "dev" ]; then
+        DATABASE_PATH="/share/j_sun/lmlm_multihop/database/gemini/musique_validation_42_1000_all_context_database.json"
+    elif [ "${SPLIT}" = "train" ]; then
+        echo "There is no train database made for musique"
+    else
+        echo "Error: SPLIT must be either 'train' or 'dev', got '${SPLIT}'"
+        exit 1
+    fi
 else
     echo "Error: DATASET must be either 'hotpotqa' or 'musique', got '${DATASET}'"
     exit 1
@@ -31,14 +50,14 @@ fi
 
 METHOD=lmlm
 MAX_TOKENS=1024
-TOTAL_COUNT=30
+TOTAL_COUNT=16
 BATCH_SIZE=16
 OUTPUT_DIR=./output
-SPLIT=dev
 SETTING=distractor
 SAVE_EVERY=64
 SEED=42
 ADAPTIVE_K=true
+START_IDX=0
 
 
 python scripts/eval_lmlm_multihop.py \
@@ -55,4 +74,5 @@ python scripts/eval_lmlm_multihop.py \
     --seed ${SEED} \
     --adaptive-k ${ADAPTIVE_K} \
     --save-every ${SAVE_EVERY}\
+    --start-index ${START_IDX}\
     --eval
